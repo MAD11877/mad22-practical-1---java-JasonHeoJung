44fe28d48b4adc3a8d7681fd8676d010607e42c8
JasonHeoJung <S10222947@connect.np.edu.sg>
2022-04-20 18:18:13 +0800

    Qns 2

--- Question2.java
+++ Question2.java
@@ -9,8 +9,13 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter height(m): ");
     double height = in.nextDouble();
+    System.out.print("Enter Weight(kg): ");
+    double weight = in.nextDouble();
+    
+    double bmi = weight/(height*height);
+    System.out.print("Bmi:"+bmi);
   }
 }
